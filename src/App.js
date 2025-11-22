df0de40e141b656d44c712a24c98db404f91d744
gouravjain22 <gouravjain2204@gmail.com>
2025-11-22 23:52:00 +0530

    implemented suffle

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Navbar from './Component/Navbar';
 import Blog from './Component/Blog'
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchData } from './redux/Data/action';
-import { useEffect } from 'react';
+import { useEffect,useRef } from 'react';
 
 function App() {
+  const effectRan = useRef(false);
   const dispatch = useDispatch()
   const getaction = ()=>{
     return dispatch(fetchData())
   }
   useEffect(()=>{
+  if (effectRan.current === false) {
     getaction();
-  },[])
+    effectRan.current = true;
+  }
+},[]);
   return (
     <>
     <Navbar title="What is the reason"></Navbar>
--- src/Component/Blog.js
+++ src/Component/Blog.js
@@ -4,11 +4,14 @@ import { Container } from './Blog.styled';
 import { useSelector } from 'react-redux';
 export default function Blog() {
   const data = useSelector((state)=>state.data.data)
+  function shuffleArray(arr) {
+  return [...arr].sort(() => Math.random() - 0.5);
+}
   return (
     <>
       {data?.length  > 0 ?  <div>
             {
-                data?.map((curData) =>{
+                shuffleArray(data)?.map((curData) =>{
                     return   <Data key={curData.id} {...curData}/>
                 })
             }   
